17a4dc5d5a17c6e05cd4ccfe1cab1b6946b9dd63
danielcillero <daniecr@kth.se>
2019-05-30 20:30:25 +0200

    1st Steps

--- Assignment2/src/assignment2/MachineLearning.java
+++ Assignment2/src/assignment2/MachineLearning.java
@@ -1,10 +1,62 @@
 package assignment2;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class MachineLearning {
+	
+	static ArrayList<Integer> timeList = new ArrayList<>(); 
+	static ArrayList<String> nameValues = new ArrayList<>();
+	static ArrayList<Double> values = new ArrayList<>(); 
+	
 	
 	public static void main(String[] args) {
 		
+		String learningFile = "measurements.csv";
 		
+		read_data(learningFile);
+		
+		for (int i=0 ; i<timeList.size() ; i++) {
+			
+		}
+		
+		
+		
+	}
+	
+	public static void read_data(String dataFile) {
+		
+		BufferedReader br = null;
+		String line = "";
+		String SplitBy = ";";
+	
+		try {
+		
+			br = new BufferedReader(new FileReader(dataFile));
+		
+		
+			while ((line = br.readLine()) != null) {
+ 	     
+			String[] measurements = line.split(SplitBy);
+			
+			if(!measurements[1].equals("rdfid")) { //Change this to make it general Creating the values before the main
+				
+				nameValues.add(measurements[2]);
+				timeList.add(Integer.parseInt(measurements[3]));
+				values.add(Double.parseDouble(measurements[4]));
+				
+			}		
+	
+		}
+		} catch (FileNotFoundException e) {
+		e.printStackTrace();
+	
+		} catch (IOException e) {
+		e.printStackTrace();
+	}		
 	}
 
 }
---
+++ Assignment2/src/assignment2/Measurement.java
@@ -0,0 +1,15 @@
+package assignment2;
+
+public class Measurement {
+	
+	int time;
+	String name;
+	Double value;
+	
+	public Measurement(int time, String name, Double value){
+		this.time = time;
+		this.name = name;
+		this.value = value;
+	}
+
+}
